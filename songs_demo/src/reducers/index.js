27f1196b84342d59d82824c15a68ddd2d939d608
Brandon <deftones2001@gmail.com>
2019-03-02 14:03:05 -0600

    fixed reducer key name

--- songs_demo/src/reducers/index.js
+++ songs_demo/src/reducers/index.js
@@ -4,7 +4,7 @@ const songsReducer = () => {
   return [
     {title: 'No Scrubs', duration: '4:05'},
     {title: 'Macarena', duration: '2:30'},
-    {title: 'All Star', durations: '3:15'},
+    {title: 'All Star', duration: '3:15'},
     {title: 'I Want it That Way', duration: '1:45'}
   ];
 };
@@ -15,5 +15,5 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
 export default combineReducers({
   songs: songsReducer,
-  selected: selectedSongReducer
+  selectedSong: selectedSongReducer
 });
